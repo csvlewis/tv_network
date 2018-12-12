ae22d0751a1bc184566871a7deef728bc2934a11
Chris Lewis <13chlewis@gmail.com>
2018-12-12 09:18:13 -0700

    Created character and character_test, iteration 1 complete

---
+++ lib/character.rb
@@ -0,0 +1,10 @@
+class Character
+  attr_reader :name,
+              :actor,
+              :salary
+  def initialize(hash)
+    @name = hash[:name]
+    @actor = hash[:actor]
+    @salary = hash[:salary]
+  end
+end
