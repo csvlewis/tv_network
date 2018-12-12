2046487fd9c20faf2d538b76cf81f79b33ba84c8
Chris Lewis <13chlewis@gmail.com>
2018-12-12 10:12:33 -0700

    Wrote network.rb, finished iteration 3

--- lib/network.rb
+++ lib/network.rb
@@ -1,2 +1,32 @@
 class Network
+  attr_reader :name,
+              :shows
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def highest_paid_actor
+    all_salaries = []
+    highest_paid_actor = []
+    @shows.each do |show|
+      show.characters.each do |character|
+        all_salaries << character.salary
+      end
+    end
+    @shows.each do |show|
+      show.characters.each do |character|
+        if character.salary == all_salaries.max
+          highest_paid_actor << character.actor
+        else
+          next
+        end
+        end
+    end
+    return highest_paid_actor[0]
+  end
 end
