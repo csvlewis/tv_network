e5b0501cf3054766a3e0bd5564b43568741b0856
Chris Lewis <13chlewis@gmail.com>
2018-12-12 09:41:38 -0700

    Created network class and wrote tests

--- test/show_test.rb
+++ test/show_test.rb
@@ -29,7 +29,7 @@ class ShowTest < Minitest::Test
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-    assert_instance_of Character, knight_rider.characters[0]
+    assert_equal 2, knight_rider.characters.count
   end
 
   def test_it_has_a_total_salary
