1238b8fb5236ad131e8697cef0370de29a473fc7
Chris Lewis <13chlewis@gmail.com>
2018-12-12 09:34:27 -0700

    Wrote show class, completed iteration 2

--- test/show_test.rb
+++ test/show_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require './lib/character'
 require './lib/show'
+require 'pry'
 
 class ShowTest < Minitest::Test
   def test_it_exists
@@ -28,7 +29,7 @@ class ShowTest < Minitest::Test
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-    assert_equal "", knight_rider.characters
+    assert_instance_of Character, knight_rider.characters[0]
   end
 
   def test_it_has_a_total_salary
