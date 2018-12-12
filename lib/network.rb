1843518f440b635aa8ff9a612cc67294fbc08556
Chris Lewis <13chlewis@gmail.com>
2018-12-12 10:23:45 -0700

    Wrote payroll method, iteration 4 complete

--- lib/network.rb
+++ lib/network.rb
@@ -12,7 +12,7 @@ class Network
 
   def highest_paid_actor
     all_salaries = []
-    highest_paid_actor = []
+    highest_paid_actor = nil
     @shows.each do |show|
       show.characters.each do |character|
         all_salaries << character.salary
@@ -21,12 +21,26 @@ class Network
     @shows.each do |show|
       show.characters.each do |character|
         if character.salary == all_salaries.max
-          highest_paid_actor << character.actor
-        else
-          next
+          highest_paid_actor = character.actor
         end
-        end
+      end
     end
-    return highest_paid_actor[0]
+    highest_paid_actor
+  end
+
+  def payroll
+    all_actors = []
+    all_salaries = []
+    @shows.each do |show|
+      show.characters.each do |character|
+        all_actors << character.actor
+      end
+    end
+    @shows.each do |show|
+      show.characters.each do |character|
+        all_salaries << character.salary
+      end
+    end
+    Hash[all_actors.zip(all_salaries)]
   end
 end
--- test/network_test.rb
+++ test/network_test.rb
@@ -45,4 +45,19 @@ class NetworkTest < Minitest::Test
     nbc.add_show(parks_and_rec)
     assert_equal "Amy Poehler", nbc.highest_paid_actor
   end
+
+  def test_it_can_report_payroll
+    nbc = Network.new("NBC")
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+    payroll = {"David Hasselhoff"=>1600000, "William Daniels"=>1000000, "Amy Poehler"=>2000000, "Nick Offerman"=>1400000}
+    binding.pry
+    assert_equal payroll, nbc.payroll
+  end
 end
